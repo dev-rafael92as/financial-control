f3c21bc74cfd7cef5032b0cae7751d721a85ece5
dev-rafael92as <77762757+dev-rafael92as@users.noreply.github.com>
2021-02-05 01:53:37 -0300

    Add Script.js + logic to add expenses, incomes and balance

---
+++ App/scripts.js
@@ -0,0 +1,137 @@
+const Modal = {
+  open(){
+      // Abrir modal
+      // Adicionar a class active ao modal
+      document
+          .querySelector('.modal-overlay')
+          .classList
+          .add('active')
+
+  },
+  close(){
+      // fechar o modal
+      // remover a class active do modal
+      document
+          .querySelector('.modal-overlay')
+          .classList
+          .remove('active')
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -10000,
+    date: '25/01/2021'
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '25/01/2021'
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '25/01/2021'
+  },
+  {
+    id: 4,
+    description: 'Água',
+    amount: + 20000,
+    date: '25/01/2021'
+  }
+]
+
+const Transaction = {
+  incomes() { 
+    
+    let income = 0;
+    //pegar todas as transações
+    //para cada transação
+      transactions.forEach(transaction => {
+      //se ela for maior que zero
+      if( transaction.amount > 0) {
+        //somar a uma variavel e retornar a variavel
+        income += transaction.amount;
+      }
+    })
+    return income
+  },
+  expenses() {
+
+    let expense = 0;
+    //pegar todas as transações
+    //para cada transação
+      transactions.forEach(transaction => {
+      //se ela for menor que zero
+      if( transaction.amount < 0) {
+        //somar a uma variavel e retornar a variavel
+        expense += transaction.amount;
+      }
+    })
+    return expense;
+  },
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
+  }
+  
+} 
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td><img src="../assets/minus.svg" alt="Remover Transação"></td>`
+    return html
+  },
+  updateBalance() {
+    document
+      .getElementById('incomeDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document
+      .getElementById('expenseDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.total())
+    
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace(/\D/g, "") // esse     /"\D"/  significa pegar todas as ocorrencias que não forem números e substituir pelo próximo valor após a virgola 
+    value = Number(value) / 100
+
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    })
+    return signal + value
+  }
+}
+
+transactions.forEach(function(transaction) {
+DOM.addTransaction(transaction)
+})
+
+DOM.updateBalance()
